b965cf54e63946775cd9c8102e86152f11dd7c2d
Gabrielrolonmatheus <129123616+Gabrielrolonmatheus@users.noreply.github.com>
2024-08-12 23:09:27 +0000

    12/08/24-2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const idade = 21;
+let idade = 21;
 const anoNascimento = 2003;
 let anoAtual = 2024;
+const nomeCompleto = "Gabriel Matheus Rolon";
 
+console.log(" Meu nome é " + nomeCompleto + " Nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo" + idade + "anos");
 anoAtual = anoAtual + 1;
 
 console.log(anoAtual);
+
+idade = idade + 1;
+console.log(idade)
